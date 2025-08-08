88876bec7867ee3a051216632c3015e73f579622
Иван Оплеснин <i.oplesnin@comagic.dev>
2025-08-08 13:01:32 +0300

    Init

--- TGbot/main.go
+++ TGbot/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+var TOKEN string
+
+func init() {
+	godotenv.Load()
+	TOKEN = os.Getenv("TOKEN")
+}
+
 func main() {
-	
+	fmt.Println(TOKEN)
 }
